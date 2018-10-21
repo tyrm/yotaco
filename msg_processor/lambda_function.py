d710fa0f99deef6134b6ca89417cf4d30508eb28
Tyr Mactire <tyr@pettingzoo.co>
2018-10-21 16:12:44 -0700

    process async via sns

---
+++ msg_processor/lambda_function.py
@@ -0,0 +1,307 @@
+from __future__ import print_function
+
+import boto3
+from boto3.dynamodb.conditions import Key, Attr
+from botocore.exceptions import ClientError
+import datetime
+import decimal
+import inflect
+import json
+import os
+import re
+import requests
+
+print('Loading function')
+dynamo = boto3.client('dynamodb')
+
+debug = os.getenv('DEBUG', 'false')
+taco_name = os.getenv('EMOJI', 'taco')
+timezone = os.getenv('TZ_OFFSET', 0)
+bot_token = os.environ['SLACK_BOT_TOKEN']
+
+
+def dynamo_add_taco(ts, index, team, channel, fromu, tou):
+    dynamodb = boto3.resource('dynamodb', region_name='us-west-2')
+
+    # Add Taco Transaction
+    transaction_table = dynamodb.Table('taco_transactions')
+    transaction_table.put_item(
+        Item={
+            'tid': str(ts) + "-" + team + "-" + channel + "-" + str(index),
+            'timestamp': decimal.Decimal(ts),
+            'channel': channel,
+            'team': team,
+            'from': fromu,
+            'to': tou
+        }
+    )
+
+    # Update Taco Counts
+    count_ids = [
+        get_cid_this_month(team),
+        get_cid_this_week(team),
+        get_cid_this_year(team),
+        get_cid_today(team)
+    ]
+
+    count_table = dynamodb.Table('taco_counts')
+    for cid in count_ids:
+        try:
+            response = count_table.update_item(
+                Key={
+                    'cid': cid
+                },
+                UpdateExpression="set #attrName = #attrName + :attrValue",
+                ExpressionAttributeNames={
+                    "#attrName": tou
+                },
+                ExpressionAttributeValues={
+                    ':attrValue': decimal.Decimal(1)
+                },
+                ReturnValues="NONE"
+            )
+            print(response)
+        except ClientError as e:
+            if e.response['Error']['Code'] == 'ValidationException':
+                # Creating new top level attribute `info` (with nested props)
+                # if the previous query failed
+                response = count_table.update_item(
+                    Key={
+                        'cid': cid
+                    },
+                    UpdateExpression="set #attrName = :attrValue",
+                    ExpressionAttributeNames={
+                        "#attrName": tou
+                    },
+                    ExpressionAttributeValues={
+                        ':attrValue': decimal.Decimal(1)
+                    },
+                    ReturnValues="NONE"
+                )
+                print(response)
+            else:
+                raise
+
+    return
+
+
+def dynamo_get_tacos_avail(user):
+    dynamodb = boto3.resource('dynamodb', region_name='us-west-2')
+    table = dynamodb.Table('taco_transactions')
+
+    response = table.scan(
+        IndexName='from-timestamp-index',
+        Select='COUNT',
+        FilterExpression=Key('from').eq(user) & Key('timestamp').gte(get_epoch(get_local_midnight()))
+    )
+
+    print(response)
+    return 5 - response['Count']
+
+
+def find_taco(message):
+    taco_re = r':' + re.escape(taco_name) + r':'
+    values = re.findall(taco_re, message)
+    return len(values)
+
+
+def find_users(message, myself):
+    values = re.findall(r'<@(U[a-zA-Z0-9]+)>', message)
+    users = list(set(values))
+
+    # Don't allow user to taco themselves
+    try:
+        myself_index = users.index(myself)
+        if debug == 'true': print('User tried to taco themself')
+        users.pop(myself_index)
+    except:
+        pass
+
+    return users
+
+
+def get_cid_this_month(team):
+    midnight = get_local_midnight()
+    return midnight.strftime('%Y-M%m') + "-" + team
+
+
+def get_cid_this_week(team):
+    midnight = get_local_midnight()
+    return midnight.strftime('%Y-W%U') + "-" + team
+
+
+def get_cid_this_year(team):
+    midnight = get_local_midnight()
+    return midnight.strftime('%Y') + "-" + team
+
+
+def get_cid_today(team):
+    midnight = get_local_midnight()
+    return midnight.strftime('%Y-%m-%d') + "-" + team
+
+
+def get_epoch(ts):
+    return int(((ts - datetime.datetime(1970, 1, 1)) - datetime.timedelta(hours=int(timezone))).total_seconds())
+
+
+def get_local_midnight():
+    st = datetime.datetime.now() + datetime.timedelta(hours=int(timezone))
+    midnight = datetime.datetime(st.year, st.month, st.day, 0, 0, 0)
+    return midnight
+
+
+def get_time_to_next_midnight():
+    st = datetime.datetime.now() + datetime.timedelta(hours=int(timezone))
+    next_midnight = datetime.datetime(st.year, st.month, st.day, 0, 0, 0) + datetime.timedelta(hours=24)
+    return next_midnight - st
+
+
+def process_tacos(tc, tu, tr, body):
+    index = 1
+    for user in tu:
+        for x in range(tc):
+            dynamo_add_taco(body['event']['event_ts'], index, body['team_id'], body['event']['channel'],
+                            body['event']['user'], user)
+            index = index + 1
+
+        send_message_you_got_taco(user, tc, body['event']['user'], body['event']['channel'], body['event']['text'])
+
+    send_message_you_sent_taco(body['event']['user'], tc, tu, tr - (index - 1))
+    return
+
+
+def respond(err, res=None):
+    return {
+        'statusCode': '400' if err else '200',
+        'body': err.message if err else json.dumps(res),
+        'headers': {
+            'Content-Type': 'application/json',
+        },
+    }
+
+
+def send_message_leaderboard(channel, tc, fromu, message):
+    p = inflect.engine()
+    text = "You received *" + str(tc) + " " + p.plural(taco_name, tc) + "* from <@" + fromu + "> in <#" + channel + ">"
+    attachment = "[{\"text\": \"" + message + "\"}]"
+
+    send_slack_message(text, channel, attachment)
+
+
+def send_message_not_enough_tacos(user, tried, tr, channel):
+    p = inflect.engine()
+    delta = get_time_to_next_midnight()
+    delta_values = divmod(delta.days * 86400 + delta.seconds, 60)
+
+    hours = delta_values[0] / 60
+    minutes = delta_values[0] % 60
+
+    text = "Whoops! You tried to give *" + str(tried) + "* " + p.plural(taco_name, tried) + ". You have *" + str(tr) + \
+           "* " + p.plural(taco_name, tr) + " left to give today. Your " + p.plural(taco_name) + \
+           " will reset in *"
+
+    if hours > 0:
+        text = text + str(hours) + " hours and "
+
+    text = text + str(minutes) + " minutes*."
+
+    send_slack_ephemeral(text, channel, user)
+
+
+def send_message_tacos_available(user, tr):
+    p = inflect.engine()
+    delta = get_time_to_next_midnight()
+    delta_values = divmod(delta.days * 86400 + delta.seconds, 60)
+
+    hours = delta_values[0] / 60
+    minutes = delta_values[0] % 60
+
+    text = "You have *" + str(tr) + "* " + p.plural(taco_name, tr) + " left to give today. Your " + \
+           p.plural(taco_name) + " will reset in *"
+
+    if hours > 0:
+        text = text + str(hours) + " hours and "
+
+    text = text + str(minutes) + " minutes*."
+
+    send_slack_message(text, user)
+
+
+def send_message_you_got_taco(user, tc, fromu, channel, message):
+    p = inflect.engine()
+    text = "You received *" + str(tc) + " " + p.plural(taco_name, tc) + "* from <@" + fromu + "> in <#" + channel + ">"
+    attachment = "[{\"text\": \"" + message + "\"}]"
+
+    send_slack_message(text, user, attachment)
+
+
+def send_message_you_sent_taco(user, tc, tu, tr):
+    p = inflect.engine()
+    text = ""
+    for tuser in tu:
+        text = text + "<@" + tuser + "> "
+    text = text + "received *" + str(tc) + " " + p.plural(taco_name, tc) + "* from you. You have *" + \
+           str(tr) + " " + p.plural(taco_name, tr) + "* left to give out today."
+
+    send_slack_message(text, user)
+
+
+def send_slack_ephemeral(message, channel, user):
+    params = {"token": bot_token, "text": message, "channel": channel, "user": user, "as_user": True}
+
+    r = requests.get('https://slack.com/api/chat.postEphemeral', params=params)
+
+
+def send_slack_message(message, channel, attachment=None):
+    params = {"token": bot_token, "text": message, "channel": channel, "as_user": True}
+    if attachment != None:
+        params['attachments'] = attachment
+
+    r = requests.get('https://slack.com/api/chat.postMessage', params=params)
+
+
+def slack_message(body):
+    # Find taco in channel messages
+    if body['event']['channel_type'] == 'channel':
+        taco_count = find_taco(body['event']['text'])
+        if taco_count > 0:
+            if debug == 'true': print("Found " + str(taco_count) + " taco(s)")
+            taco_users = find_users(body['event']['text'], body['event']['user'])
+            if len(taco_users) > 0:
+                my_tacos_avail = dynamo_get_tacos_avail(body['event']['user'])
+                if debug == 'true': print("you have " + str(my_tacos_avail) + " taco(s)")
+
+                taco_tries = taco_count * len(taco_users)
+                if my_tacos_avail >= taco_tries:
+                    if debug == 'true': print("you can give these tacos")
+                    process_tacos(taco_count, taco_users, my_tacos_avail, body)
+                else:
+                    send_message_not_enough_tacos(body['event']['user'], taco_tries, my_tacos_avail,
+                                                  body['event']['channel'])
+    elif body['event']['channel_type'] == 'im' and body['authed_users'][0] != body['event']['user']:
+        p = inflect.engine()
+        if body['event']['text'] == p.plural(taco_name):
+            my_tacos_avail = dynamo_get_tacos_avail(body['event']['user'])
+            send_message_tacos_available(body['event']['user'], my_tacos_avail)
+
+    return
+
+
+# main
+def lambda_handler(event, context):
+    midnight = datetime.datetime.now() + datetime.timedelta(hours=int(timezone))
+    print(midnight.strftime('%Y-%m-%d %H:%M:%S'))
+    print(get_epoch(midnight))
+
+    for record in event['Records']:
+        # Get POST body
+        try:
+            body = json.loads(record['Sns']['Message'])
+        except ValueError as e:
+            return respond(e)
+
+        # Route Body
+        if debug == 'true':
+            print("Got body: " + json.dumps(body, indent=2))
+
+        slack_message(body)
